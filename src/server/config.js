b0eca028e72335a49b8bf60f9d12d41e25f635a9
Emiliano Policardo <emilianopolicardo@gmail.com>
2019-09-30 18:24:03 -0300

    Index Page. Min: 46.43

--- src/server/config.js
+++ src/server/config.js
@@ -1,17 +1,47 @@
+//Llamo un paquete Path y lo almaceno como constante
 const path = require('path');
 const exphbs = require('express-handlebars');
+const morgan = require('morgan');
+const multer = require('multer');
+const express = require('express');
+const routes = require('../routes/index');
 
 
 module.exports = app => {
 
     //Settings
+
     //Especifico el puerto a utilizar
     app.set('port', process.env.PORT || 3000);
-   
+    //Digo donde estan las vistas
+    app.set('views', path.join(__dirname, 'views'));
+    app.engine('.hbs', exphbs({
+        defaultLayout: 'main',
+        partialsDir: path.join(app.get('views'), 'partials'),
+        layoutsDir: path.join(app.get('views'), 'layouts'),
+        extname: '.hbs',
+        helpers: require('./helpers')
+
+    }));
+    app.set('view engine', '.hbs');
+
     //Middleware
 
-    //Routes
+    app.use(morgan('dev'));
+    app.use(multer({
+        dest: path.join(
+            __dirname, '../public/upload/temp'
+        )
+    }).single('image'));
 
+    app.use(express.urlencoded({
+        extended: false
+    }));
+    app.use(express.json());
+
+    //Routes
+    routes(app);
+    app.use
     //ErrorHandlers
 
 
