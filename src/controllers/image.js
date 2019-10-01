7a43bf42e42b9c9be9b15a2206589c7299793eb7
Emiliano Policardo <emilianopolicardo@gmail.com>
2019-10-01 00:02:52 -0300

    Subo Imagen, error al mover. 1.55:00

--- src/controllers/image.js
+++ src/controllers/image.js
@@ -1,21 +1,36 @@
+const path = require('path');
 const ctrl = {};
+const { randomNumber } = require('../helpers/libs');
+const fs = require('fs-extra');
+
+
+
+
 ctrl.index = (req, res) => {
     res.send('index')
 };
 
-ctrl.create = (req, res)=>{
+ctrl.create = async (req, res) => {
+    const imgURL = randomNumber();
+    console.log(imgURL);
+    const imageTempPath = req.file.path;
+    const ext = path.extname(req.file.originalname).toLowerCase();
+    const targetPath = path.resolve('src/public/upload/${ImgURL}${ext}')
+    if (ext === '.png' || ext === '.gif') {
+        await fs.rename(imageTempPath, targetPath);
+    }
+    res.send("Works");
+};
+
+ctrl.like = (req, res) => {
 
 };
 
-ctrl.like = (req, res)=>{
+ctrl.comment = (req, res) => {
 
 };
 
-ctrl.comment = (req, res)=>{
-
-};
-
-ctrl.remove = (req, res)=>{
+ctrl.remove = (req, res) => {
 
 };
 module.exports = ctrl;
