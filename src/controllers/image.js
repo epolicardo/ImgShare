0ab7785f2603f0f8d08d5a231950b5855756d3be
Emiliano Policardo <emilianopolicardo@gmail.com>
2019-10-03 20:32:19 -0300

    Subiendo imagenes, validando nombres y guardando en MongoDB

--- src/controllers/image.js
+++ src/controllers/image.js
@@ -3,26 +3,49 @@ const ctrl = {};
 const { randomNumber } = require('../helpers/libs');
 const fs = require('fs-extra');
 
-
+//Esto es lo mismo que sin el index, ya que node por defecto, cuando importa, busca el index
+//const { Image } =  require('../models/index');
+const { Image } = require('../models');
 
 
 ctrl.index = (req, res) => {
     res.send('index')
 };
 
-ctrl.create = async (req, res) => {
-    const imgUrl = randomNumber();
-    console.log(imgUrl);
-    const imageTempPath = req.file.path;
-    const ext = path.extname(req.file.originalname).toLowerCase();
-    const targetPath = path.resolve("src/public/upload/"+imgUrl+ext);
-    //const targetPath = path.resolve('src/public/upload/$imgUrl$ext');
-    
-    console.log(targetPath);
-    if (ext === '.png' || ext === '.gif') {
-        await fs.rename(imageTempPath, targetPath);
-    }
-    res.send("Creacion Correcta");
+ctrl.create =  (req, res) => {
+
+    const  saveImage = async() => {
+        const imgUrl = randomNumber();
+        const images = await Image.find({ filename: imgUrl });
+
+        if (images.length > 0) {
+            saveImage();
+        }else{
+            console.log(imgUrl);
+            const imageTempPath = req.file.path;
+            const ext = path.extname(req.file.originalname).toLowerCase();
+            const targetPath = path.resolve("src/public/upload/" + imgUrl + ext);
+            //const targetPath = path.resolve('src/public/upload/$imgUrl$ext');
+           
+            if (ext === '.png' || ext === '.gif') {
+                await fs.rename(imageTempPath, targetPath);
+                const newImage = new Image({
+                title: req.body.title,
+                filename: imgUrl + ext,
+                description: req.body.description,
+            })
+            const imageSaved = await newImage.save();
+            res.send('Works');
+        
+            } else {
+                await fs.unlink(imageTempPath);
+                res.status(500).json({ error: 'Only format accepted' });
+            }
+        }
+    };
+
+    saveImage();
+
 };
 
 ctrl.like = (req, res) => {
---
+++ src/models/index.js
@@ -0,0 +1,8 @@
+module.exports= {
+
+    Image: require('./image')
+    //User: require('./image')
+   // Image: require('./image')
+    //Image: require('./image')
+
+};
