ed5c2c2dca6c8a5f7ab0636c3409bb7d9aeb4580
Emiliano Policardo <emilianopolicardo@gmail.com>
2019-10-03 18:20:37 -0300

    Mover imagenes reparado

--- src/controllers/image.js
+++ src/controllers/image.js
@@ -11,15 +11,18 @@ ctrl.index = (req, res) => {
 };
 
 ctrl.create = async (req, res) => {
-    const imgURL = randomNumber();
-    console.log(imgURL);
+    const imgUrl = randomNumber();
+    console.log(imgUrl);
     const imageTempPath = req.file.path;
     const ext = path.extname(req.file.originalname).toLowerCase();
-    const targetPath = path.resolve('src/public/upload/${ImgURL}${ext}')
+    const targetPath = path.resolve("src/public/upload/"+imgUrl+ext);
+    //const targetPath = path.resolve('src/public/upload/$imgUrl$ext');
+    
+    console.log(targetPath);
     if (ext === '.png' || ext === '.gif') {
         await fs.rename(imageTempPath, targetPath);
     }
-    res.send("Works");
+    res.send("Creacion Correcta");
 };
 
 ctrl.like = (req, res) => {
