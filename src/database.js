e96e32bbbc67073e540d85ce25121c8bb23fc0d5
Emiliano Policardo <emilianopolicardo@gmail.com>
2019-10-18 02:03:40 -0300

    Configurando DB Remota

--- src/database.js
+++ src/database.js
@@ -1,9 +1,12 @@
 const mongoose = require('mongoose');
+const keys = require("./keys");
 
-const { database } = require('./keys');
-mongoose.connect(database.URI, {
-        useNewUrlParser: true
+//const { database } = require('./keys');
+
+mongoose.connect(keys.database.URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
     })
-    .then(db => console.log('DB Is conected'))
+    .then(db => console.log('DB Is conected to ' + keys.database.name))
     .catch(err => console.error(err));
 ;
--- src/keys.js
+++ src/keys.js
@@ -1,5 +1,9 @@
 module.exports = {
     database: {
-        URI:'mongodb://localhost/imgShare'
+/*         URI:'mongodb+srv://epolicardo:30782195@epolicardo-hh3da.mongodb.net/test?retryWrites=true&w=majority',
+        name: "MongoDB Atlas"
+ */ 
+       URI:'mongodb://localhost/imgShare', 
+       name: "LocalhostDB"
     }
 }
