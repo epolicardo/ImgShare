a42592afa07b644f6bfe989c1a1d53a83d87db91
Emiliano Policardo <emilianopolicardo@gmail.com>
2019-11-06 20:29:27 -0300

    Update keys.js
    Password expuesta

--- src/keys.js
+++ src/keys.js
@@ -1,6 +1,6 @@
 module.exports = {
     database: {
         URI:'mongodb://localhost/imgShare',
-        URIRemota: 'mongodb+srv://epolicardo:30782195@epolicardo-hh3da.mongodb.net/imgShare?retryWrites=true&w=majority'
+        URIRemota: 'mongodb+srv://epolicardo:<password>@epolicardo-hh3da.mongodb.net/imgShare?retryWrites=true&w=majority'
     }
 }
